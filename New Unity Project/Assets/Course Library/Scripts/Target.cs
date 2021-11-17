89411cdcf164d9d8fb6f3ea48793c799c1da6c32
Mathew Harris <10959101@uvu.edu>
2021-11-16 22:23:34 -0700

    Target basic Code
    Coded target script to launch object at random x location, with random rotation and random force.

--- New Unity Project/Assets/Course Library/Scripts/Target.cs
+++ New Unity Project/Assets/Course Library/Scripts/Target.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    //variables
+    private Rigidbody targetRb;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //assign targetRb
+        targetRb = GetComponent<Rigidbody>();
+
+        //add force to move up
+        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
+
+        //add force to rotate
+        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+
+        //create random spawn point
+        transform.position = new Vector3(Random.Range(-4, 4), -6);
     }
 
     // Update is called once per frame
